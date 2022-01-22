68437073495146f43874c4aada1c7660ce597e82
mayals <developer.web.dj@gmail.com>
2022-01-23 02:24:12 +0300

    fix errors in models.py

--- exchange/models.py
+++ exchange/models.py
@@ -8,16 +8,16 @@ class Category(models.Model):
         return self.name
 
 class Currency(models.Model):
-    name = models.Charfield(max_length=32,blank=False,null=True)
-    code = models.Charfield(max_length=3,blank=False,null=True,unique=True)
+    name = models.CharField(max_length=32,blank=False,null=True)
+    code = models.CharField(max_length=3,blank=False,null=True,unique=True)
     def _str__(self):
         return self.name
 
 class Transaction(models.Model):
-    category = models.ForiengKey(Category,on_delete= models.SET_NULL,null=True,blank=False)
+    category = models.ForeignKey(Category,on_delete= models.SET_NULL,null=True,blank=False)
     currency =  models.ForeignKey(Currency,on_delete= models.PROTECT,null=True,blank=False)
     date_created =  models.DateTimeField(auto_now_add= True,auto_now=False)
-    amount = models.DcimalField(max_digits=15,decimal_places=2,null=True,blank=False)
+    amount = models.DecimalField(max_digits=15,decimal_places=2,null=True,blank=False)
     description = models.TextField(blank=True,null=True)
     def _str__(self):
         return f"{self.amount}-{self.currency.code}-{self.date_created}"
